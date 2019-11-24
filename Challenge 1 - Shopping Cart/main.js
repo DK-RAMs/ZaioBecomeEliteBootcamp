6ecee7380802b19424c06c01feb4390a2d139d22
DKSLATERLOL <dkslater101@gmail.com>
2019-11-24 02:35:03 +0200

    Add files via upload
    Commit 2 - Templates of Modals completed
    
    As well as updates to images used for displaying Hickies Lace and background of website

--- Challenge 1 - Shopping Cart/main.js
+++ Challenge 1 - Shopping Cart/main.js
@@ -1,11 +1,51 @@
-var availableColors = [ "Orange", "Yellow", "Slate Blue", "Steel Blue", "Dark Salmon", "Deep Sky Blue", "Lawn Green", "White", "Deep Pink", "Light Salmon", "Medium Sea Green", "Gray", "Brown", "Turquoise", "Light Gray", "Crimson", "Blue Violet", "Black" ];
+var availableColors = ["Orange", "Yellow", "Slate Blue", "Steel Blue", "Dark Salmon", "Deep Sky Blue", "Lawn Green", "White", "Deep Pink", "Light Salmon", "Medium Sea Green", "Gray", "Brown", "Turquoise", "Light Gray", "Crimson", "Blue Violet", "Black"];
+
+/************** All HTML code that is injected via Javascript using inserts etc **************/
 
 var colorWheel = document.getElementById("colorselector");
 
 var currentIndex = -1;
 
+var main = function(){
+    
+}
+
+var checkSpecToCart = function(a){
+    if(currentIndex < 0){
+        /***** Do all functions and code here*/
+    }
+    else{
+        
+    }
+}
+
+var checkSelection = function(a){
+    if(document.getElementById("color " + a).style.borderColor == "black"){
+         return null;
+    }
+    else{
+        return document.getElementById("color " + a).style.borderColor = "white";
+    }
+}
+
+var resetonHover = function(a){
+    if(document.getElementById("color " + a).style.borderColor == "black"){
+        return null;
+    }
+    else{
+        return document.getElementById("color " + a).style.borderColor = "lightgrey";
+    }
+}
+
+
 var resetSelection = function(a){
-    document.getElementById("color " + a).style.borderColor = "White";
+    document.getElementById("color " + a).style.borderColor = "white";
+    document.getElementById("color " + a).onmouseover = function(){
+        resetonHover(a);
+    }
+    document.getElementById("color " + a).onmouseout = function(){
+        checkSelection(a);
+    }
 }
 
 var setcolor = function(a){
@@ -23,7 +63,6 @@ var setcolor = function(a){
     }
     
 }
-
 
 var create = function(str){
     let fragment = document.createDocumentFragment();
@@ -45,21 +84,34 @@ var convertToColor = function(a){
     return finalstring;
 }
 
+var a = [1];
 
-setcolor(currentIndex);
-
-
-if(availableColors.length < 1){
-    let frag = create('<span>None available</span>');
-    document.getElementById("colorselector").appendChild(frag);
-}
-else{
-    for(let i = 0; i < availableColors.length; i++){
-        let colorid = '\"color ' + i + '\"';
-        let frag = create('<div id=' + '"color ' + i + '" class="colorblock d-inline" data-toggle="tooltip" data-placement="top" title="' + availableColors[i] + '"></div>');
-        colorWheel.appendChild(frag);
-        document.getElementById("color " + i).style.color = convertToColor(i);
-        document.getElementById("color " + i).style.backgroundColor = convertToColor(i);
-        document.getElementById("color " + i).addEventListener("click", function(){ setcolor(i)});
+var main = function() {
+    setcolor(currentIndex);
+    if(availableColors.length < 1){
+        let frag = createDiv('<div>None available</div>');
+        document.getElementById("colorselector").appendChild(frag);
     }
+    else{
+        for(let i = 0; i < availableColors.length; i++){
+            let colorid = '\"color ' + i + '\"';
+            let frag = create('<div id=' + '"color ' + i + '" class="colorblock d-inline" data-toggle="tooltip" data-placement="top" title="' + availableColors[i] + '"></div>');
+            colorWheel.appendChild(frag);
+            document.getElementById("color " + i).style.color = convertToColor(i);
+            document.getElementById("color " + i).style.backgroundColor = convertToColor(i);
+            document.getElementById("color " + i).addEventListener("click", function(){ setcolor(i)});
+        }
+    }
+    
+    
+    
+    $(document).ready(function(){
+        $('[data-toggle="tooltip"]').tooltip();
+        $('').onclick = function(){
+            
+        }
+    });
 }
+
+
+main();
